4a5139c78bb85b599455b92939fc5d20b7f5ce63
Qiusi-Shen <qiusi@ucla.edu>
2018-02-19 23:06:13 -0800

    first commit

---
+++ Actor.h
@@ -0,0 +1,11 @@
+#ifndef ACTOR_H_
+#define ACTOR_H_
+
+#include "GraphObject.h"
+#include "GameConstants.h"
+// Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+
+class StudentWorld;
+
+
+#endif // ACTOR_H_
